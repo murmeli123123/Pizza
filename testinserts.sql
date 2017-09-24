c8daf9d2c0c43e6a32920b11222d218a08c3236d
murmeli123123 <32156767+murmeli123123@users.noreply.github.com>
2017-09-24 18:56:38 +0300

    Add files via upload
    Testi insertit

---
+++ testinserts.sql
@@ -0,0 +1,7 @@
+INSERT INTO planet VALUES(1, 'Testi planeetta', 'Testaava planeetta');
+INSERT INTO place VALUES(1, 'Testi paikka', 'Täällä testataan', 1);
+INSERT INTO actiontable VALUES(1, 'Jack opens the door and something happens');
+INSERT INTO objecttype VALUES(1, 'DOOR');
+INSERT INTO object VALUES(1, 'JAIL', 'ei action', 1, 1, 1, NULL);
+INSERT INTO object VALUES(2, 'SHIP', 'action', 1, 1, 1, 1);
+INSERT INTO player VALUES(1, 0, 20, 1);

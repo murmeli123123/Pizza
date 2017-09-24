81b4d037d19c8a588eb2a07f096fbd5f1fb13049
Andrei Vasilev <amlk158@gmail.com>
2017-09-24 11:04:38 +0300

    First comit

--- Peli.py
+++ Peli.py
@@ -7,10 +7,57 @@ db = mysql.connector.connect(host = "localhost",
                               buffered = True)
 cursor = db.cursor()
 
+#main function is a game core!! Function call on the end of code!
+
+def mainFunc():
+  # Initialize player location
+
+    location = "something location"
+    command = ""
+                            # location is current location
+                            # command is action verb
+                            # target object 
+
+    while command!="quit" and location!="EXIT":
+
+        input_command=input("> ").split()
+
+        if len(input_string)>=1:
+            action = input_command[0].lower()
+        else:
+            action = ""     
+        if len(input_string)>=2:
+            target = input_command[len(input_command)-1].lower() #lower make the string to lowercase
+        else:
+            target = ""     
+
+
+            # get
+            # move
+            # inventory
+        if action=="inventory" or action=="i":
+              inventoryFunc()
+
+
+
+
+def inventoryFunc():
+    cur = db.cursor()
+    sql = "SELECT ........................'"
+    cur.execute(sql)
+    if cur.rowcount>=1:
+        print("You carry the following items:")
+        for row in cur.fetchall() :
+            print (" - " + row[0])
+    else:
+        print("You don't carry anything.")
+    return
+
+
 command = ''
 while command != 'stop':
     # print(command)
-    command = input("What to do next: ").split(' ')
+    command = input("> ").split(' ')
     if command[0] == 'open':
         sql = "SELECT object.typeID, objecttype.name \
                 FROM object inner join objecttype \
@@ -23,3 +70,4 @@ while command != 'stop':
                 command = 'stop'
         
     
+main()

b4131c72b654130c41f735fb60dcde36d1df7ce5
Heppatytto99 <mihail.manner@gmail.com>
2017-09-25 12:21:06 +0300

    open funktio

--- Open.py
+++ Open.py
@@ -1,33 +1,58 @@
-import mysql.connector
+'''import mysql.connector
 
 db = mysql.connector.connect(host = "localhost",
                               user = "dbuser",
                               passwd = "salasana",
                               db = "pizzaDB",
                               buffered = True)
-                              
-while command != 'stop':
-    command = input("What to do next: ").split(' ')
-    if command[0].upper() == 'OPEN':
-        sql = "SELECT object.objectID, object.name, objecttype.objectname, object.actionID \
-                FROM objecttype join object \
-                WHERE object.typeID = objecttype.typeID"
-        cursor.execute(sql)
-        result = cursor.fetchall()
-        objectname = ''
+cursor = db.cursor()
+# Location = Player.placeID
+location = 2
+# Objecttype = objecttype.typename , Minkälainen objekti halutaan avata: ovi, laatikko yms.
+objecttype = "door"
+objecttype.upper()
+# Objectname = object.name, mikä objekti halutaan avata jos useita.
+name = "jail3"
+'''
+
+def openFunc(location, objecttype, *objectname):
+    # For getting the actions
+    def getAction(result):
         for x in result:
-            # jos paikassa on monia door objekteja lisää ne merkkijonoon
-            if command[1].upper() != x[1] and command[1].upper() == x[2]:
-                objectname += x[1] + ', '
-            # jos sekä objekti että objektin nimi on oikein ja objektilla on action
-            elif command[1].upper() == x[1] and command[2].upper() == x[2] and x[3] != None:
                 sql = "SELECT actiontable.description \
                         FROM actiontable WHERE actiontable.actionID = %s" % x[3]
                 cursor.execute(sql)
                 result = cursor.fetchall()
-                # prittaa objektin action
                 for x in result:
                     print(x[0])
-        # Tulosta kaikki tämän paikan objektien nimet
-        if len(objectname) > 1:
-            print("There is multiple objects: " + objectname)
+
+    sql = "SELECT object.objectID, object.name, objecttype.typename, object.actionID \
+            FROM objecttype join object \
+            WHERE object.typeID = objecttype.typeID and object.placeID = %i and objecttype.typename = '%s'" % (location, objecttype)
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    multiple = ''
+
+    # If there is only one objecttype in place
+    if len(result) == 1:
+        getAction(result)
+    else:
+        for x in result:
+            # If there is no objectname defined
+            if objectname == ():
+                multiple += ' ' + x[1]
+            else:
+                # If player has defined which door to open and If object has actionID
+                if x[1] == name.upper():
+                    if x[3] != None:
+                        getAction(result)
+                    else:
+                        print("Jack opens the %s door" % x[1])
+                # If objectname doesn't match any objects in place
+                else:
+                    multiple += ' ' + x[1]
+        # Print place objects
+        if len(multiple) > 1:
+            print("There is multiple objects: " + multiple)
+
+openFunc(location, objecttype, name)

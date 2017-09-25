8c6abb4fd37fe563b684675457384cdff707db21
Andrei Vasilev <amlk158@gmail.com>
2017-09-25 11:44:25 +0300

    Versio 00000.1

--- Peli.py
+++ Peli.py
@@ -9,24 +9,24 @@ cursor = db.cursor()
 
 #main function is a game core!! Function call on the end of code!
 
-def mainFunc():
+def main():
   # Initialize player location
 
     location = "something location"
-    command = ""
+    action = ""
                             # location is current location
                             # command is action verb
                             # target object 
 
-    while command!="quit" and location!="EXIT":
+    while action!="quit" and location!="EXIT":
 
         input_command=input("> ").split()
 
-        if len(input_string)>=1:
+        if len(input_command) >= 1:
             action = input_command[0].lower()
         else:
             action = ""     
-        if len(input_string)>=2:
+        if len(input_command) >= 2:
             target = input_command[len(input_command)-1].lower() #lower make the string to lowercase
         else:
             target = ""     
@@ -35,39 +35,96 @@ def mainFunc():
             # get
             # move
             # inventory
-        if action=="inventory" or action=="i":
-              inventoryFunc()
+        print(action, target)
+        if action == "get" or "take":
+            getFunc( target )
 
+def getFunc(target):
 
-
-
-def inventoryFunc():
     cur = db.cursor()
-    sql = "SELECT ........................'"
+    sql = "SELECT name FROM Item"
     cur.execute(sql)
-    if cur.rowcount>=1:
-        print("You carry the following items:")
-        for row in cur.fetchall() :
-            print (" - " + row[0])
-    else:
-        print("You don't carry anything.")
-    return
+    rez = cur.fetchall()
+    for i in rez:
+        if i[0] == target:
+            cur = db.cursor()
+            sql = "SELECT itemID FROM Item WHERE name = '%s' " % (target)
+            cur.execute(sql)
+            rez = cur.fetchall()
+
+            cur = db.cursor()
+            sql = "UPDATE item SET playerID = 1 WHERE itemID = '%i'  " % (rez[0][0])
+            cur.execute(sql)
+            db.commit()
+            print ("You take a new item: " + target)
+        else:
+           print(target + " not found!")
 
 
-command = ''
-while command != 'stop':
-    # print(command)
-    command = input("> ").split(' ')
-    if command[0] == 'open':
-        sql = "SELECT object.typeID, objecttype.name \
-                FROM object inner join objecttype \
-                WHERE object.typeID = objecttype.typeID"
-        cursor.execute(sql)
-        result = cursor.fetchall()
-        for x in result:
-            if x[1] == 'DOOR':
-                print("Avasit oven JIHUU")
-                command = 'stop'
-        
-    
+
+def getFunc(target):
+
+    cur = db.cursor()
+    sql = "SELECT name FROM Item"
+    cur.execute(sql)
+    rez = cur.fetchall()
+    for i in rez:
+        if i[0] == target:
+            cur = db.cursor()
+            sql = "SELECT itemID FROM Item WHERE name = '%s' " % (target)
+            cur.execute(sql)
+            rez = cur.fetchall()
+
+            cur = db.cursor()
+            sql = "UPDATE item SET playerID = 1 WHERE itemID = '%i'  " % (rez[0][0])
+            cur.execute(sql)
+            db.commit()
+            print ("You take a new item: " + target)
+        else:
+           print(target + " not found!")
+
+
+
+def dropFunc(target):
+
+    cur = db.cursor()
+    sql = "SELECT name FROM Item WHERE playerID = 1"
+    cur.execute(sql)
+    rez = cur.fetchall()
+    for i in rez:
+        if i[0] == target:
+            cur = db.cursor()
+            sql = "SELECT itemID FROM Item WHERE name = '%s' " % (target)
+            cur.execute(sql)
+            rez = cur.fetchall()
+
+            cur = db.cursor()
+            sql = "UPDATE item SET playerID = 0 WHERE itemID = '%i'  " % (rez[0][0])
+            cur.execute(sql)
+            db.commit()
+            print ("You droped item: " + target)
+        else:
+           print(target + " not found in you inventory!")
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 main()

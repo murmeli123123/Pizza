5d59b65e2bc4f1ee0f7ad9a0707e75978dfaa875
Andrei Vasilev <amlk158@gmail.com>
2017-09-25 13:38:49 +0300

    Last commit

--- Peli.py
+++ Peli.py
@@ -32,16 +32,18 @@ def main():
             target = ""     
 
 
-            # get
-            # move
-            # inventory
-        # print(action, target)
+        if action == "get":      #get item
+            if target!="": 
+                getFunc(target)
+            else:
+              print("It's just a verb!, Try again!")
 
-        if action == "get":
-            getFunc(target)
-
-        # elif action == "drop":
-        #     dropFunc(target)
+        elif action == "drop": 
+            if target!="": 
+                getFunc(target)
+            else:
+                print("It's just a verb!, Try again!")   
+            
 
 
 def getFunc(target):
@@ -80,7 +82,40 @@ def getFunc(target):
 
 
 
+def dropFunc(target):
+    item = 0
+    cur = db.cursor()
+    sql = "SELECT name FROM Item"
+    cur.execute(sql)
+    rez = cur.fetchall()
+    for i in rez:
+        if i[0] == target:
+            item = 1
 
+
+    cur = db.cursor()
+    sql = "SELECT itemID FROM Item WHERE name = '%s' " % (target)
+    cur.execute(sql)
+    rez = cur.fetchall()
+
+    cur = db.cursor()
+    sql = "SELECT playerID FROM Item WHERE name = '%s' " % (target)
+    cur.execute(sql)
+    playerid = cur.fetchall()
+
+
+    if item == 1:
+        if playerid[0][0] == 1: 
+            cur = db.cursor()
+            sql = "UPDATE item SET playerID = NULL WHERE itemID = '%i'  " % (rez[0][0])
+            cur.execute(sql)
+            db.commit()
+            print ("You droped item: " + target)
+        else:
+            print("You just droped it")
+        
+    else:
+      print("Item not found")
 
 
 

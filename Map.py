dd10d51acb7b6b653ccb144b1f578d6f78981b11
takatukka-Jack <32158907+takatukka-Jack@users.noreply.github.com>
2017-09-26 13:20:12 +0300

    Toimiva kartta valmis

---
+++ Map.py
@@ -0,0 +1,48 @@
+import mysql.connector
+
+db = mysql.connector.connect(host = "localhost",
+                              user = "dbuser",
+                              passwd = "salasana",
+                              db = "pizzaDB",
+                              buffered = True)
+cursor = db.cursor()
+
+# Kartan malli
+def mapbase():
+    base = list(" _______\n|\t|\n|\t|\n|_______|")
+    return base
+
+# Ilmansuunnat Minne pelaaja voi mennä olemastaan ruudusta
+def moving():
+    sql = "SELECT movingTable.direction \
+            FROM player, place, movingTable \
+            WHERE player.placeID = place.placeID \
+            AND place.placeID = movingTable.placeID"
+    cursor.execute(sql)
+    move = cursor.fetchall()
+    movements = str(move)
+    return movements
+
+# Kartta siitä ruudusta missä pelaaja on
+def currentmap():
+    base = mapbase()
+    movements = moving()
+    for x in movements:
+        if x == "N":
+            base[4] = "N"
+        elif x == "E":
+            base[13] = "E"
+        elif x == "W":
+            base[15] = "W"
+        elif x == "S":
+            base[21] = "S"
+    for x in base:
+        print(x, end='')
+    print('\n')
+
+
+command = ''
+while command != 'stop':
+    command = input("What to do next: ").split(' ')
+    if command[0].upper() == 'MAP' or "M":
+        currentmap()
